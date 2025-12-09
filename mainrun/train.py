2cffb100b0f832b28ec41217f0fb3adfb13d373b
Jared Collette <jaredmcollette@gmail.com>
2025-12-09 07:16:38 +0000

    Mainrun auto checkpoint

--- mainrun/train.py
+++ mainrun/train.py
@@ -14,7 +14,7 @@ import structlog
 
 @dataclass
 class Hyperparameters:
-    block_size: int = 512
+    block_size: int = 128
     batch_size: int = 64
     vocab_size: int = 16_000
     n_layer: int = 4
